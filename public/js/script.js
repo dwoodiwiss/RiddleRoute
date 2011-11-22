1252611ae50f0445ef4d06c8c277b2970fcc8873
Darren <darrenwoodiwiss@gmail.com>
2011-11-22 20:54:14 +0000

    updated js a bit to be more logical

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,43 @@
 $(document).ready(function loader() {
     console.log("this is from the loader function"); 
     hider();
+    //tester();
+    answerMatch();
 });
 
 function hider() {
-        console.log("this is from the hider function");
-        $('#screen1').removeClass('hidden');
+    console.log("this is from the hider function");
+    $('#screen1').removeClass('hidden');
 };
 
-function other(){
-    console.log("this is from the other function");
+function tester() {
+    //var $answer = $(this).get(['answer.input']);
+    console.log($answer);
+
+    $answer = $('#answer').val(name); 
+    console.log($answer);
+}
+
+function answerMatch(){
+    // temporary var
+    $answer = "temp";
+
+    // riddle 1
+    $answer1 = /Jane\ Austin/i;
+    // riddle 2
+    $answer2 = "data2";
+    // riddle 3
+    $answer3 = "data3";
+    // riddle 4
+    $answer4 = "data4";
+    // riddle 5
+    $answer5 = "data5";
+
+    if ($answer == $answer1 ) {
+        // send to correct answer page
+        console.log('Riddle 1 is correct!');
+    }
+    else {
+        // send to incorrect answer page
+    }
 };
