a79c2ddd3fc7759fa6924282fc42fc8b615deb7c
Darren <darrenwoodiwiss@gmail.com>
2011-11-22 17:45:38 +0000

    updated javascript loader and function caller

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,13 @@
-$(document).ready(function() {
-
-    $('#screen1').removeClass('hidden');
-
-    
-
+$(document).ready(function loader() {
+    console.log("this is from the loader function"); 
+    hider();
 });
+
+function hider() {
+        console.log("this is from the hider function");
+        $('#screen1').removeClass('hidden');
+};
+
+function other(){
+    console.log("this is from the other function");
+};
