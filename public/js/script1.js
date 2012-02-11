396152f9d04105361fd09777b9e5349360eb524e
Darren <darrenwoodiwiss@gmail.com>
2012-02-11 14:12:42 +0000

    removed old script

--- public/js/script1.js
+++
@@ -1,8 +0,0 @@
-$("#answerForm").submit(function() {
-    if ($("input:first").val() == "Jane Austin") {
-        $("#result").text("Correct!").show().fadeOut(1000);
-        return false;
-    }
-    $("#result").text("Incorrect").show().fadeOut(1000);
-    return false;
-});
